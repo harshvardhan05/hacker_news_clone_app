0583290809c0a703f57023a513725fcbce2afb28
Harshvardhan <hv050694@gmail.com>
2021-07-12 22:30:16 +0530

    first commit

---
+++ pages/stories.js
@@ -0,0 +1,24 @@
+import view from "../util/view.js";
+
+export default async function Stories(path) {
+  const stories = await getStories(path);
+  console.log(stories);
+  const hasStories = stories.length > 0;
+
+  view.innerHTML = `<div>
+  ${hasStories ? stories.map((story) => JSON.stringify(story)) : "no stories"}
+  </div>`;
+}
+
+async function getStories(path) {
+  const isHomeRoute = path === "/";
+  const isNewRoute = path === "/new";
+  if (isHomeRoute) {
+    path = "/news";
+  } else if (isNewRoute) {
+    path = "/newest";
+  }
+  const response = await fetch(`https://node-hnapi.herokuapp.com${path}`);
+  const stories = await response.json();
+  return stories;
+}
